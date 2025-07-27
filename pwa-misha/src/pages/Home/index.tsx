a3006309567e3edd092ff97fc3e5ee5cc41641a8
Misha Higham <misha.higham@hotmail.co.uk>
2025-07-27 14:10:41 +0100

    feat: add glow to MAH logo

--- pwa-misha/src/pages/Home/index.tsx
+++ pwa-misha/src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 //import { useState } from 'react'
+import './index.css'
 import mah_logo_no_bg_black_border from '@assets/mah_logo_no_bg_black_border.png'
 
 function Home() {
     return (
         <div className='flex'>
             <div className='flex flex-col gap-3 items-center'>
-                <img src={mah_logo_no_bg_black_border} alt="MAH" className='w-64 h-auto pb-10'></img>
+                <img src={mah_logo_no_bg_black_border} alt="MAH" className='logo pb-10'></img>
                 <p className="font-['playfair'] font-medium italic text-4xl">hi i am</p>
                 <h1 className="font-['playwrite']">Misha Higham</h1>
             </div>
